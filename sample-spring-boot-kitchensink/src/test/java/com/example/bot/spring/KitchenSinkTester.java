ba68214eda208256a7de943a2c5a048b5396fcf0
Daniel Yip <daniel_hin1996@Daniels-MacBook-Pro.local>
2017-09-22 11:52:33 +0800

    Test #1

--- sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/KitchenSinkTester.java
+++ sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/KitchenSinkTester.java
@@ -74,18 +74,21 @@ public class KitchenSinkTester {
 		assertThat(!thrown).isEqualTo(true);
 		assertThat(result).isEqualTo("def");
 	}
+	
+	@Test
+	public void testFound() throws Exception {
+		boolean thrown = false;
+		String result = null;
+		try {
+			result = this.databaseEngine.search("I am fine");
+		} catch (Exception e) {
+			thrown = true;
+		}
+		assertThat(!thrown).isEqualTo(true);
+		assertThat(result).isEqualTo("Great!");
+	}
+	}
+
 }
 
-@Test
-public void testFound() throws Exception {
-	boolean thrown = false;
-	String result = null;
-	try {
-		result = this.databaseEngine.search("I am fine");
-	} catch (Exception e) {
-		thrown = true;
-	}
-	assertThat(!thrown).isEqualTo(true);
-	assertThat(result).isEqualTo("Great!");
-}
-}
+
